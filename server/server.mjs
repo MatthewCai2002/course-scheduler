941c6bc51c08af9e90fb2d080785b236f1616f2c
forbet <matthewcaiuni@gmail.com>
2023-05-05 09:33:02 -0700

    fixed bug with connecting to mongodb

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,3 @@
-// imports
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
@@ -12,5 +11,5 @@ app.use(express.json())
 app.use(route)
 
 app.listen(PORT, () => {
-    console.log('Server is running on port: ${PORT}')
+    console.log(`Server is running on port: ${PORT}`)
 })
